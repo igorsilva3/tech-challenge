92d6c426eed030db6876a753239f8a4bb2171e25
Igor Silva <igorbr2018@gmail.com>
2022-11-16 19:31:46 -0300

    Update account service

--- src/services/AccountServices/GetAccountService/index.ts
+++ src/services/AccountServices/GetAccountService/index.ts
@@ -8,13 +8,13 @@ interface IGetAccountServiceDTO {
   }
 }
 
-interface ICreateAccountService {
-  execute(data: IGetAccountServiceDTO): Promise<Account | null>
+interface IGetAccountService {
+  execute(data: IGetAccountServiceDTO): Promise<Account>
 }
 
-export class GetAccountService implements ICreateAccountService {
-  async execute(data: IGetAccountServiceDTO): Promise<Account | null> {
-    const account = await prisma.account.findUnique({
+export class GetAccountService implements IGetAccountService {
+  async execute(data: IGetAccountServiceDTO): Promise<Account> {
+    const account = await prisma.account.findUniqueOrThrow({
       where: {
         id: data.where.id
       }
